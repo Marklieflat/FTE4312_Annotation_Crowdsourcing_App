476573dbf8c4d0a0a7037707c762f9dc3ac68c88
Marklieflat <1737499105@qq.com>
2023-11-30 22:37:17 +0800

    [Mark] Update project.sol

--- project.sol
+++ project.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
 // Need to be done:
 // 1. Deposit function should be fixed to let the user specify the amount of Ether to deposit
@@ -14,93 +14,143 @@ pragma solidity ^0.8.0;
 // 5-6: Zekai Sunday
 
 contract RWRC {
-    // Struct for representing a task
     struct Task {
         uint id;
         string description;
         uint reward;
         address requester;
-        address worker;
+        mapping(address => WorkerStatus) workers;
+        uint workerCount;
         bool isCompleted;
-        bool isAccepted;
-        string solutionHash; // Using a hash for solution
+        address[] workerAddresses; // Array to track worker addresses
+        string[] solutionHashes; // Array of solution hashes
+    }
+
+    struct WorkerStatus {
+        bool isAssigned;
+        bool hasSubmitted;
     }
 
     uint public taskCount;
     mapping(uint => Task) public tasks;
-    mapping(address => uint) public reputation; // Worker reputation
+    mapping(address => uint) public reputation;
 
-    uint constant minimumReputation = 10; // Minimum reputation required
+    uint constant minimumReputation = 10;
+    uint constant maxTasks = 50;
 
-    // Events for logging contract actions
     event TaskCreated(uint taskId, address requester);
     event TaskAssigned(uint taskId, address worker);
     event TaskCompleted(uint taskId, address worker, string solutionHash);
     event TaskAccepted(uint taskId, bool isAccepted);
     event ReputationUpdated(address worker, uint newReputation);
+    event DepositMade(address depositor, uint amount);
 
-    // Function to create a new task
     function createTask(string memory _description, uint _reward) public {
+        require(taskCount < maxTasks, "Max task limit reached");
         taskCount++;
-        tasks[taskCount] = Task(taskCount, _description, _reward, msg.sender, address(0), false, false, "");
+        tasks[taskCount].id = taskCount;
+        tasks[taskCount].description = _description;
+        tasks[taskCount].reward = _reward;
+        tasks[taskCount].requester = msg.sender;
+        tasks[taskCount].workerCount = 0;
+        tasks[taskCount].isCompleted = false;
+        // The mapping and array will be automatically initialized to their default values
+
         emit TaskCreated(taskCount, msg.sender);
     }
 
-    // Function for workers to assign themselves a task
-    function assignTask(uint _taskId) public {
+    function assignTask(uint _taskId, address _worker) public {
         if (reputation[msg.sender] == 0) {
             // Initialize reputation to 50 for new workers
             reputation[msg.sender] = 50;
         }
-        require(reputation[msg.sender] >= minimumReputation, "Insufficient reputation");
+        require(reputation[_worker] >= minimumReputation, "Insufficient reputation");
         Task storage task = tasks[_taskId];
         require(task.requester != address(0), "Task does not exist");
-        require(task.worker == address(0), "Task already assigned");
-
-        task.worker = msg.sender;
-        emit TaskAssigned(_taskId, msg.sender);
-    }
-
-    // Function for workers to submit a completed task
-    function completeTask(uint _taskId, string memory _solutionHash) public {
-        Task storage task = tasks[_taskId];
-        require(task.worker == msg.sender, "Not the assigned worker");
         require(!task.isCompleted, "Task already completed");
 
-        task.isCompleted = true;
-        task.solutionHash = _solutionHash;
-        emit TaskCompleted(_taskId, msg.sender, _solutionHash);
+        task.workers[_worker].isAssigned = true;
+        task.workerAddresses.push(_worker); // Add worker address to the array
+        task.workerCount++;
+        emit TaskAssigned(_taskId, _worker);
     }
 
-    // Function for requesters to evaluate task solutions
+    function completeTask(uint _taskId, address _worker, string memory _solutionHash) public {
+        Task storage task = tasks[_taskId];
+        require(task.workers[_worker].isAssigned, "Worker not assigned");
+        require(!task.workers[_worker].hasSubmitted, "Solution already submitted");
+
+        task.workers[_worker].hasSubmitted = true;
+        task.solutionHashes.push(_solutionHash);
+        emit TaskCompleted(_taskId, _worker, _solutionHash);
+
+        if (task.solutionHashes.length == task.workerCount) {
+            task.isCompleted = true;
+            // Further logic for finalizing task completion
+        }
+    }
+
     function evaluateTask(uint _taskId, bool _isAccepted) public {
         Task storage task = tasks[_taskId];
         require(task.requester == msg.sender, "Not the task requester");
         require(task.isCompleted, "Task not yet completed");
 
-        task.isAccepted = _isAccepted;
         if (_isAccepted) {
-            payable(task.worker).transfer(task.reward);
-            taskCount--;
-            updateReputation(task.worker, true);
+            uint rewardPerWorker = task.reward / task.workerCount;
+            for (uint i = 0; i < task.workerAddresses.length; i++) {
+                address workerAddress = task.workerAddresses[i];
+                payable(workerAddress).transfer(rewardPerWorker);
+                updateReputation(workerAddress, true);
+            }
         } else {
-            updateReputation(task.worker, false);
+            for (uint i = 0; i < task.workerAddresses.length; i++) {
+                updateReputation(task.workerAddresses[i], false);
+            }
         }
+
         emit TaskAccepted(_taskId, _isAccepted);
+        delete tasks[_taskId];
+        taskCount--;
     }
 
-    // Internal function to update worker reputation
+
+
     function updateReputation(address _worker, bool _success) internal {
         if (_success) {
-            reputation[_worker] += 5; // Increase reputation
+            reputation[_worker] += 5;
         } else {
             if (reputation[_worker] > 5) {
-                reputation[_worker] -= 5; // Decrease reputation, but not below 0
+                reputation[_worker] -= 5;
             }
         }
         emit ReputationUpdated(_worker, reputation[_worker]);
     }
 
-    // Function to deposit Ether into the contract as rewards
-    function deposit() public payable {}
+    function deposit() public payable {
+        emit DepositMade(msg.sender, msg.value);
+    }
+
+    // New function to get the contract's Ether balance
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    // New function to get the number of tasks of a worker
+    function getWorkerTaskCount(address _worker) public view returns (uint) {
+        uint count = 0;
+        for (uint i = 1; i <= taskCount; i++) {
+            if (tasks[i].workers[_worker].isAssigned) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // New function to withdraw Ether from the contract
+    function withdraw(uint _amount) public {
+        require(msg.sender == address(this), "Unauthorized");
+        require(_amount <= address(this).balance, "Insufficient balance");
+        payable(msg.sender).transfer(_amount);
+    }
 }
+
